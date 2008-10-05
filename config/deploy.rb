18d17f435f83ad6a8917963dc3e5864f75260acf
Johan Eckerström <johan@duh.se>
2008-10-05 15:25:19 +0000

    sync

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,15 @@
-# This defines a deployment "recipe" that you can feed to switchtower
-# (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
-# (among other things) the deployment of your application.
+set :application, "set your application name here"
+set :repository,  "set your repository location here"
 
-# The application name
-set :application, "wikimum"
-set :repository, "svn+ssh://genau.jage.se/export/storage/imum/svn/imum-site/trunk"
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
 
-# Everything is running as my use
-set :use_sudo, false
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
 
-# One server for everything
-role :web, "hera.jage.se"
-role :app, "hera.jage.se"
-role :db,  "genau.jage.se", :primary => true
-
-# Were the application lives
-set :deploy_to, "/var/www/users/jage/wikimum"
-
-# =============================================================================
-# TASKS
-# =============================================================================
-
-desc "Start the spinner daemon"
-task :spinner, :roles => :app do
-  run "#{current_path}/script/spin"
-end
-
-desc "Restart Ruby-processes"
-task :restart, :roles => :app do
-  run "#{current_path}/script/process/reaper"
-end
-
-desc "Show a maintenance screen for all requests"
-task :disable_web do
-  # Read in the file and make sure the variables are available
-  maintenance = render('config/templates/maintenance', :until => ENV['UNTIL'], :reason => ENV['REASON'])
-  # Send the file to the server
-  put maintenance, "#{shared_path}/system/maintenance.html", :mode => 0644
-
-  on_rollbak do
-    delete "#{shared_path}/system/maintenance.html"
-  end
-end
+role :app, "your app-server here"
+role :web, "your web-server here"
+role :db,  "your db-server here", :primary => true
--- lib/markup.rb
+++ lib/markup.rb
@@ -8,13 +8,18 @@ class Markup
     wikified = text.gsub(/\[\[([\d\w\sÅÄÖåäö_:-]{1,35})\]\]/i) do
       "{{#{$1}}}"
     end
+    # Link URLs
+    #wikified.gsub!(/((ftp|http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)/) do
+    #   "<a href=\"#{$1}\">#{$1}</a>"
+    #end
         
     #html_escape	=	{ '&' => '&amp;', '"' => '&quot;', '>' => '&gt;', '<' => '&lt;' }
     #wikified.gsub!(/[&"><]/) { |special| html_escape[special] }
 
     case markup.to_s.downcase
     when 'textile'
-      RedCloth.new(wikified).to_html
+      textile = RedCloth.new(wikified)
+      textile.to_html
     when 'markdown'
       BlueCloth.new(wikified).to_html
     else
