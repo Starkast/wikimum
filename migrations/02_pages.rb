47199139223e4dd1dbcf5d02ea7be2c1aeebfd40
Johan Eckerström <johan@duh.se>
2013-03-02 20:53:56 +0100

    Add more columns to pages

---
+++ migrations/02_pages.rb
@@ -0,0 +1,14 @@
+Sequel.migration do
+  change do
+    alter_table(:pages) do
+      add_column :shorthand_title, String, unique: true
+      add_column :title_char, String
+      add_column :compiled_content, String, text: true
+      add_column :comment, String, text: true
+      add_column :compiled_comment, String, text: true
+      add_column :description, String, text: true
+      add_column :compiled_description, String, text: true
+      add_column :markup, String
+    end
+  end
+end
