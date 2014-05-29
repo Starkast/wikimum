83e5ef4e8fd05acff9ddd9c3373485104dd57337
Johan Eckerström <johan@duh.se>
2014-05-29 21:07:20 +0200

    Disconnect databases before forking
    Each child needs to find it's own connections

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,15 +8,11 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  DB.disconnect
 end
 
 after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
 end
