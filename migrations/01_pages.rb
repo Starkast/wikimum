0cb3401b95cf6c515abad340624dd4d08cc0976b
Patrik Ragnarsson <patrik@starkast.net>
2014-05-29 12:46:58 +0200

    Tabs to spaces and newlines at end of files

--- migrations/01_pages.rb
+++ migrations/01_pages.rb
@@ -1,9 +1,9 @@
 Sequel.migration do
-	change do
-		create_table(:pages) do
-			primary_key :id
-			String :title
-			Text :content
-		end
-	end
+  change do
+    create_table(:pages) do
+      primary_key :id
+      String :title
+      Text :content
+    end
+  end
 end
