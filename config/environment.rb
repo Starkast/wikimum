fd3fb495bff427592a68a5789daa9666478d9a4f
Johan Eckerström <johan@duh.se>
2014-05-29 09:29:34 +0200

    Cleanup config.ru
    Move environment setup to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,18 @@
+require 'sinatra/base'
 require 'sequel'
 
+if ENV.fetch('RACK_ENV') == 'development'
+  require 'dotenv'
+  Dotenv.load
+
+  $stdout.sync = true
+  $stderr.sync = true
+end
+
 DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
+
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'services'
+require 'models'
+require 'controllers'
