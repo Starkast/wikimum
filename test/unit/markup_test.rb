0c5a96337dc01a8359fa307ed953c01b62189d7b
Johan Eckerström <johan@duh.se>
2014-06-02 21:42:52 +0200

    Fix tests for HTML::Pipeline
    That was introduced in b1b5c86376eb6fe646704315c9f82b13256a2cc0

--- test/unit/markup_test.rb
+++ test/unit/markup_test.rb
@@ -2,16 +2,18 @@ require 'minitest/autorun'
 require_relative '../../lib/services/markup'
 
 class MarkupTest < MiniTest::Unit::TestCase
-  def test_wikify_content
+  def test_wiki_link_filter
     content = '[[Server]]'
     output = '[Server](/Server)'
 
-    assert_equal output, Markup.wikify_content(content)
+    doc = HTML::Pipeline.parse(content)
+
+    assert_equal output, WikiLinkFilter.call(doc, {})
   end
 
   def test_wikified_with_markdown
     content = '[[Server]]'
-    html_output = "<p><a href=\"/Server\">Server</a></p>\n"
+    html_output = "<p><a href=\"/Server\">Server</a></p>"
 
     assert_equal html_output, Markup.to_html(content)
   end
