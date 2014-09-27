44467cff0e785563bd5e15af983625c3667b5d8f
Johan Eckerström <johan@duh.se>
2014-09-27 16:01:24 +0200

    Rewrite WikiLinkFilter
    It will now convert to HTML instead of converting first to Markdown.
    This will solve some Markdown issues like not being able to use `>
    word` for block quotes.

--- lib/services/markup.rb
+++ lib/services/markup.rb
@@ -3,8 +3,8 @@ require 'html/pipeline'
 class Markup
   def self.to_html(content)
     pipeline = HTML::Pipeline.new [
-      WikiLinkFilter,
       HTML::Pipeline::MarkdownFilter,
+      WikiLinkFilter,
     ]
     result = pipeline.call(content)
     result.fetch(:output).to_s
@@ -12,10 +12,18 @@ class Markup
 end
 
 class WikiLinkFilter < HTML::Pipeline::Filter
+
+  WIKI_LINK_REGEXP = /\[\[(?<link>[\d\w\sÅÄÖåäö_:-]{1,35})\]\]/i
+
   def call
-    content = doc.to_s
-    content.gsub(/\[\[([\d\w\sÅÄÖåäö_:-]{1,35})\]\]/i) do
-      "[#{$1}](/#{$1})"
+    search_text_nodes(doc).each do |node|
+      content = node.to_html
+      content.gsub!(WIKI_LINK_REGEXP) do |word|
+        link = word.match(WIKI_LINK_REGEXP)[:link]
+        %(<a href="/#{link}">#{link}</a>)
+      end
+      node.replace(content)
     end
+    doc.to_html
   end
 end
--- test/unit/markup_test.rb
+++ test/unit/markup_test.rb
@@ -3,17 +3,42 @@ require_relative '../../lib/services/markup'
 
 class MarkupTest < MiniTest::Unit::TestCase
   def test_wiki_link_filter
-    content = '[[Server]]'
-    output = '[Server](/Server)'
+    content     = %([[Server]])
+    html_output = %(<a href="/Server">Server</a>)
 
     doc = HTML::Pipeline.parse(content)
 
-    assert_equal output, WikiLinkFilter.call(doc, {})
+    assert_equal html_output, WikiLinkFilter.call(doc, {})
+  end
+
+  def test_wiki_link_filter_with_multiple_links_on_one_row
+    content     = %([[Server]] [[Hardware]])
+    html_output = %(<a href="/Server">Server</a> <a href="/Hardware">Hardware</a>)
+
+    doc = HTML::Pipeline.parse(content)
+
+    assert_equal html_output, WikiLinkFilter.call(doc, {})
+  end
+
+  def test_wiki_link_filter_with_multiple_links_on_two_rows
+    content     = %([[Server]]\n[[Hardware]])
+    html_output = %(<a href="/Server">Server</a>\n<a href="/Hardware">Hardware</a>)
+
+    doc = HTML::Pipeline.parse(content)
+
+    assert_equal html_output, WikiLinkFilter.call(doc, {})
   end
 
   def test_wikified_with_markdown
-    content = '[[Server]]'
-    html_output = "<p><a href=\"/Server\">Server</a></p>"
+    content     = %([[Server]])
+    html_output = %(<p><a href="/Server">Server</a></p>)
+
+    assert_equal html_output, Markup.to_html(content)
+  end
+
+  def test_markdown_blockquote
+    content     = %(> test)
+    html_output = %(<blockquote>\n<p>test</p>\n</blockquote>)
 
     assert_equal html_output, Markup.to_html(content)
   end
