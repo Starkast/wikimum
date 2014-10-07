3a05b41c55f722b9c4f7f9bf5d6d657366f9299a
Johan Eckerström <johan@duh.se>
2014-10-07 20:48:02 +0200

    Add workaround for #11
    This fixes #11 by simply not using Nokogiri::XML::Node#replace to
    replace the wikilink content.

--- lib/services/markup.rb
+++ lib/services/markup.rb
@@ -16,14 +16,10 @@ class WikiLinkFilter < HTML::Pipeline::Filter
   WIKI_LINK_REGEXP = /\[\[(?<link>[\d\w\sÅÄÖåäö_:-]{1,35})\]\]/i
 
   def call
-    search_text_nodes(doc).each do |node|
-      content = node.to_html
-      content.gsub!(WIKI_LINK_REGEXP) do |word|
-        link = word.match(WIKI_LINK_REGEXP)[:link]
-        %(<a href="/#{link}">#{link}</a>)
-      end
-      node.replace(content)
+    content = doc.to_s
+    content.gsub(WIKI_LINK_REGEXP) do |word|
+      link = word.match(WIKI_LINK_REGEXP)[:link]
+      %(<a href="/#{link}">#{link}</a>)
     end
-    doc.to_html
   end
 end
