3bfbe98822547083e882655a9d5f9ac8662096fb
Johan Eckerström <johan@duh.se>
2014-10-15 18:51:14 +0200

    Enable sinatra logging

--- lib/controllers/base_controller.rb
+++ lib/controllers/base_controller.rb
@@ -5,6 +5,7 @@ require 'haml'
 class BaseController < Sinatra::Base
   set :views, -> { "views/#{self.name.downcase.sub('controller', '')}" }
   set :haml, layout: :'/../layout', format: :html5, escape_html: true
+  set :logging, true
 
   use Rack::Flash
 
