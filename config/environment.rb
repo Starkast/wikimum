aad36ac75dc9ff5d4008e2371938c2c30dfa68e3
Johan Eckerström <johan@duh.se>
2015-01-04 12:21:38 +0100

    Enable warnings in development
    Warnings from gems will be filtered.

--- config/environment.rb
+++ config/environment.rb
@@ -13,6 +13,9 @@ DB = Sequel.connect(ENV.fetch('DATABASE_URL'))
 if ENV.fetch('RACK_ENV') == 'development'
   require 'logger'
   DB.logger = Logger.new($stdout)
+
+  # Enable warnings, but not for gems
+  require 'tool/warning_filter'
 end
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
