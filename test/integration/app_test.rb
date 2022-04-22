Starkast/wikimum

commit 674fb86ce3e9863f488c782953c231ee0bdf07da

Do not list concealed pages (#330)
* Page method `with_concealed_if(bool)` to list concealed pages
* Move Page.search to dataset extension
* Show a lock for concealed pages in lists
* Rubocop: Enable and auto correct dot position https://docs.rubocop.org/rubocop/cops_layout.html#layoutdotposition

lib/controllers/page_controller.rb
@@ -30,22 +30,25 @@ class PageController < BaseController
 
   get '/list' do
     @page_title = "Innehållsförteckning"
-    @page_groups = Page.order(:title_char, :title).to_hash_groups(:title_char)
+    @page_groups = Page.order(:title_char, :title)
+      .with_concealed_if(starkast?)
+      .to_hash_groups(:title_char)
     haml :index
   end
 
   get '/latest' do
     @page_title = "Senast ändrad"
-    @page_groups = Page.order(:updated_on).reverse.
-      eager_graph(:author).
-      add_graph_aliases(date: [:pages, :date, Sequel.lit("DATE(updated_on)")]).
-      to_hash_groups(:date)
+    @page_groups = Page.order(:updated_on).reverse
+      .with_concealed_if(starkast?)
+      .eager_graph(:author)
+      .add_graph_aliases(date: [:pages, :date, Sequel.lit("DATE(updated_on)")])
+      .to_hash_groups(:date)
     haml :latest
   end
 
   get '/search' do
     @page_title = "Sökresultat"
-    @pages = Page.search(params[:q])
+    @pages = Page.with_concealed_if(starkast?).search(params[:q])
     @q = params[:q]
 
     case @pages.count
@@ -85,7 +88,7 @@ class PageController < BaseController
   end
 
   get '/:slug/edit' do
-    @page = Page.find(slug: slug)
+    @page = Page.with_concealed_if(starkast?).find(slug: slug)
     @page_title = "Ändrar #{@page.title}"
     restrict_concealed(@page)
     haml :edit

test/integration/app_concealed_pages_test.rb
@@ -1,0 +1,67 @@
+# frozen_string_literal: true
+
+require_relative "../test_helper"
+require_relative "../integration_test_helper"
+
+class AppConcealedPagesTest < Minitest::Test
+  include Rack::Test::Methods
+
+  OUTER_APP = Rack::Builder.parse_file("config.ru").first
+
+  def app
+    OUTER_APP
+  end
+
+  def setup
+    user = User.create(email: "test@test")
+    Page.create(title: "Concealed", author: user, concealed: true)
+  end
+
+  def teardown
+    Page[title: "Concealed"].destroy
+    User[email: "test@test"].destroy
+  end
+
+  def login_as_starkast
+    env "rack.session", { login: "not used", user_id: rand(100), starkast: true }
+  end
+
+  def test_concealed
+    get "/Concealed"
+    follow_redirect!
+    assert last_response.body.include?("Not authorized")
+    assert last_response.ok?
+  end
+
+  def test_concealed_logged_in_as_starkast
+    login_as_starkast
+    get "/Concealed"
+    assert last_response.ok?
+  end
+
+  def test_latest
+    get "/latest"
+    refute last_response.body.include?("Concealed")
+    assert last_response.ok?
+  end
+
+  def test_latest_logged_in_as_starkast
+    login_as_starkast
+    get "/latest"
+    assert last_response.body.include?("Concealed")
+    assert last_response.ok?
+  end
+
+  def test_list
+    get "/list"
+    refute last_response.body.include?("Concealed")
+    assert last_response.ok?
+  end
+
+  def test_list_logged_in_as_starkast
+    login_as_starkast
+    get "/list"
+    assert last_response.body.include?("Concealed")
+    assert last_response.ok?
+  end
+end

.rubocop.yml
@@ -13,3 +13,5 @@ Style/FrozenStringLiteralComment:
   Enabled: true
 Style/MutableConstant:
   Enabled: true
+Layout/DotPosition:
+  Enabled: true

lib/models/page.rb
@@ -5,6 +5,24 @@ class Page < Sequel::Model
   one_to_many :revisions
   many_to_one :author, class: :User
 
+  SEARCH_IN_COLUMNS = %i(title content description).freeze
+
+  dataset_module do
+    def with_concealed_if(allowed_to_see_concealed)
+      return self if allowed_to_see_concealed
+
+      self.exclude(concealed: true)
+    end
+
+    def search(query)
+      terms = query.to_s.strip.split
+
+      return self.where { false } if terms.empty?
+
+      self.full_text_search(SEARCH_IN_COLUMNS, terms, rank: true)
+    end
+  end
+
   def before_create
     self.slug = Slug.slugify(self.title)
   end
@@ -30,15 +48,5 @@ class Page < Sequel::Model
 
   def calculate_sha1
     Digest::SHA1.hexdigest(self.to_hash.values.join)
-  end
-
-  def self.search(query)
-    terms = query.to_s.strip.split
-
-    return [] if terms.empty?
-
-    columns  = %i(title content description)
-
-    self.dataset.full_text_search(columns, terms, rank: true).to_a
   end
 end

views/page/index.haml
@@ -11,5 +11,7 @@
     - pages.each do |page|
       %li
         %a{ href: "#{page.slug}" }= page.title
+        - if page.concealed
+          🔐
         = "(#{page.revision})"
         %span.description= page.description

views/page/latest.haml
@@ -5,6 +5,8 @@
     - pages.each do |page|
       %li
         %a{ href: "#{page.slug}" }= page.title
+        - if page.concealed
+          🔐
         = "(#{page.revision})"
         %span.latest_item_information
           = "av #{page.author.login}"

views/page/search.haml
@@ -3,6 +3,8 @@
   - @pages.each do |page|
     %li
       %a.page-link{ href: "#{page.slug}" }= page.title
+      - if page.concealed
+        🔐
       %span.description= page.description
 
 :javascript

commit 257af591af3951368457e7586b32c064db112d7a

Fix `/:slug/edit` bug
Regression from https://github.com/Starkast/wikimum/pull/330

lib/controllers/page_controller.rb
@@ -88,7 +88,7 @@ class PageController < BaseController
   end
 
   get '/:slug/edit' do
-    @page = Page.with_concealed_if(starkast?).find(slug: slug)
+    @page = Page.find(slug: slug)
     @page_title = "Ändrar #{@page.title}"
     restrict_concealed(@page)
     haml :edit

test/integration/app_concealed_pages_test.rb
@@ -13,13 +13,13 @@ class AppConcealedPagesTest < Minitest::Test
   end
 
   def setup
-    user = User.create(email: "test@test")
-    Page.create(title: "Concealed", author: user, concealed: true)
+    @user = User.create(email: "test@test")
+    @page = Page.create(title: "Concealed", author: @user, concealed: true)
   end
 
   def teardown
-    Page[title: "Concealed"].destroy
-    User[email: "test@test"].destroy
+    @page.destroy
+    @user.destroy
   end
 
   def login_as_starkast
@@ -36,6 +36,12 @@ class AppConcealedPagesTest < Minitest::Test
   def test_concealed_logged_in_as_starkast
     login_as_starkast
     get "/Concealed"
+    assert last_response.ok?
+  end
+
+  def test_concealed_edit_view_logged_in_as_starkast
+    login_as_starkast
+    get "/Concealed/edit"
     assert last_response.ok?
   end
 

test/integration/app_test.rb
@@ -13,17 +13,30 @@ class AppTest < Minitest::Test
   end
 
   def setup
-    user = User.create(email: "test@test")
-    Page.create(title: "Test åäö", author: user)
+    @page_title = "Test åäö"
+    @user = User.create(email: "test@test")
+    @page = Page.create(title: @page_title, author: @user)
   end
 
   def teardown
-    Page[title: "Test åäö"].destroy
-    User[email: "test@test"].destroy
+    @page.destroy
+    @user.destroy
   end
 
   def test_root
     get "/"
+    assert last_response.ok?
+  end
+
+  def test_page
+    get "/#{URI.encode(@page.slug)}"
+    assert last_response.body.include?(@page_title)
+    assert last_response.ok?
+  end
+
+  def test_page_edit_view
+    get "/#{URI.encode(@page.slug)}/edit"
+    assert last_response.body.include?(@page_title)
     assert last_response.ok?
   end
 