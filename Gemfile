Starkast/wikimum

commit acf57ec06ddb9ff3403acf56ababaa58f8cd3f43

Run migrations before starting application
Hopefully helps with https://github.com/Starkast/wikimum/issues/506

Procfile
@@ -1,1 +1,1 @@
-web: bundle exec puma -C config/puma.rb
+web: bin/web_start

bin/db_migrate
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# https://fly.io/docs/laravel/the-basics/customizing-deployments/#release-command
+# Since https://github.com/Starkast/wikimum/issues/506 "release_command" is not used
 # https://fly.io/docs/reference/configuration/#run-one-off-commands-before-releasing-a-deployment
 
 set -e

bin/web_start
@@ -1,0 +1,6 @@
+#!/bin/sh
+
+set -e
+set -u
+
+bundle exec puma -C config/puma.rb

fly.toml
@@ -3,8 +3,8 @@ app = "wikimum"
 [build]
   builder = "heroku/buildpacks:20"
 
-[deploy]
-  release_command = "bin/fly_release_command"
+[processes]
+  web = "/bin/sh -c 'bin/db_migrate && bin/web_start'"
 
 [env]
   PRIMARY_REGION = "ams"

commit eab51bb1730cf95c4ae58186577ebcc8f771408d

Use `overman` + avoid dangling processes after running tests
Fix regression introduced by https://github.com/Starkast/wikimum/commit/acf57ec06ddb9ff3403acf56ababaa58f8cd3f43

Gemfile
@@ -27,7 +27,7 @@ gem 'rubocop', '~> 1.56.0', require: false
 gem 'dyno_metadata'
 
 group :development do
-  gem 'foreman'
+  gem 'overman'
   gem 'localhost'
 end
 

Procfile
@@ -1,1 +1,1 @@
-web: bin/web_start
+web: bundle exec puma -C config/puma.rb