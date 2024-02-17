826d301c5772812af5f7543c7f01dc752e818ab6
Patrik Ragnarsson <patrik@starkast.net>
2024-02-18 00:50:31 +0100

    Use HTML-Pipeline v3
    Close https://github.com/Starkast/wikimum/pull/581

--- lib/services/markup.rb
+++ lib/services/markup.rb
@@ -1,31 +1,30 @@
 # frozen_string_literal: true
 
-require 'html/pipeline'
+require 'html-pipeline'
 require 'github/markup'
 
 class Markup
   def self.to_html(content)
-    pipeline = HTML::Pipeline.new [
+    text_filters = [
       MarkdownFilter,
       WikiLinkFilter,
     ]
-    result = pipeline.call(content)
-    result.fetch(:output).to_s
+    pipeline = HTMLPipeline.new(text_filters:)
+    pipeline.to_html(content.to_s, context: {}, result: {})
   end
 end
 
-class MarkdownFilter < HTML::Pipeline::TextFilter
+class MarkdownFilter < HTMLPipeline::TextFilter
   def call
     GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, text)
   end
 end
 
-class WikiLinkFilter < HTML::Pipeline::Filter
-
+class WikiLinkFilter < HTMLPipeline::TextFilter
   WIKI_LINK_REGEXP = /\[\[(?<link>[\d\w\sÅÄÖåäö:-]{1,35})\]\]/i.freeze
 
   def call
-    content = doc.to_s
+    content = text
     content.gsub(WIKI_LINK_REGEXP) do |word|
       link = word.match(WIKI_LINK_REGEXP)[:link]
       %(<a href="/#{link}">#{link}</a>)
