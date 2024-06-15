502cd419f9672608cfc0c2265cc4edb0776a7b04
Patrik Ragnarsson <patrik@starkast.net>
2024-06-15 16:37:48 +0200

    SQLite3 as the database

--- migrations/11_page_search.rb
+++ migrations/11_page_search.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 Sequel.migration do
-  change do
+  up do
+    # SQLite is not using indexes for full text search
+    # https://sqlite.org/fts5.html
+    next if database_type == :sqlite
+
     alter_table(:pages) do
       add_full_text_index %i(title content description)
+    end
+  end
+
+  down do
+    next if database_type == :sqlite
+
+    alter_table(:pages) do
+      drop_index %i(title content description)
     end
   end
 end
