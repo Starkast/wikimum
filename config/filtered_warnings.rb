0dc30aef0f0fc0c09708723811e173e68063cb8e
Patrik Ragnarsson <patrik@starkast.net>
2024-12-21 16:02:17 +0100

    Silence default gem removal warning about rdoc (#704)
    A bit distracting when running the tests.

--- config/filtered_warnings.rb
+++ config/filtered_warnings.rb
@@ -4,6 +4,7 @@ require "warning"
 
 # See https://github.com/jeremyevans/ruby-warning#usage--
 ignores = %i[
+  default_gem_removal
   method_redefined
   mismatched_indentations
   missing_ivar
@@ -12,11 +13,14 @@ ignores = %i[
   unused_var
 ] # this list is sorted alphabetically
 
-# TODO: add :default_gem_removal when this PR is release (warning >1.4.0)
-# https://github.com/jeremyevans/ruby-warning/pull/24
-
 Gem.path.each do |path|
   Warning.ignore(ignores, path)
 end
 
 $VERBOSE = true
+
+# default_gem_removal
+#
+#   github-markup-5.0.1/lib/github/markup/rdoc.rb:2:
+#   warning: rdoc was loaded from the standard library, but will no longer be
+#   part of the default gems starting from Ruby 3.5.0
