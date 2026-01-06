f6873e1fa6c154e29a614b5f10cbc5a03205d79c
Patrik Ragnarsson <patrik@starkast.net>
2026-01-06 15:39:36 +0100

    Use Zeitwerk (#867)
    https://github.com/fxn/zeitwerk

--- lib/models.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require 'models/page'
-require 'models/revision'
-require 'models/user'
--- lib/services.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'services/markup'
-require 'services/title'
-require 'services/authorize'
-require 'services/slug'
-require 'services/authorized_user'
-require 'services/app_metadata'
